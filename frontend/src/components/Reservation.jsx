9b3577af5886429e1320b8e290d4c524e3161b30
guptabhavya07 <bhavyagupta7102003@gmail.com>
2024-09-06 11:40:31 +0530

    last commit

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -17,7 +17,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://tastybites-11.onrender.com/api/v1/reservation/send",
+        "http://localhost:4000/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
